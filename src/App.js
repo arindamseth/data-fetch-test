819d8d85af35a2bc6710c9815d1a45b30a57568a
Arindam Seth <arindam.seth@hcl.com>
2020-03-17 17:47:08 +0530

    Using effects hook efficiently

--- src/App.js
+++ src/App.js
@@ -5,17 +5,18 @@ import "./App.css";
 function App() {
   const [data, setData] = useState({ hits: [] });
   const [query, setQuery] = useState("redux");
+  const [url, setUrl] = useState(
+    `https://hn.algolia.com/api/v1/search?query=redux`
+  );
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios(
-        "https://hn.algolia.com/api/v1/search?query=redux"
-      );
+      const result = await axios(url);
       setData(result.data);
     };
 
     fetchData();
-  }, []);
+  }, [url]);
 
   return (
     <Fragment>
@@ -24,6 +25,14 @@ function App() {
         value={query}
         onChange={event => setQuery(event.target.value)}
       />
+      <button
+        type="button"
+        onClick={() =>
+          setUrl(`https://hn.algolia.com/api/v1/search?query=${query}`)
+        }
+      >
+        Search
+      </button>
       <ul>
         {data.hits.map(item => (
           <li key={item.objectID}>
