1caa8dfeb76856c48d09986c6164288b9ebc2116
Arindam Seth <arindam.seth@hcl.com>
2020-03-17 17:57:11 +0530

    Handling errors and useing forms

--- src/App.js
+++ src/App.js
@@ -8,11 +8,20 @@ function App() {
   const [url, setUrl] = useState(
     `https://hn.algolia.com/api/v1/search?query=redux`
   );
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios(url);
-      setData(result.data);
+      setIsError(false);
+      setIsLoading(true);
+      try {
+        const result = await axios(url);
+        setData(result.data);
+      } catch (error) {
+        setIsError(true);
+      }
+      setIsLoading(false);
     };
 
     fetchData();
@@ -20,26 +29,31 @@ function App() {
 
   return (
     <Fragment>
-      <input
-        type="text"
-        value={query}
-        onChange={event => setQuery(event.target.value)}
-      />
-      <button
-        type="button"
-        onClick={() =>
-          setUrl(`https://hn.algolia.com/api/v1/search?query=${query}`)
-        }
+      <form
+        onSubmit={event => {
+          setUrl(`https://hn.algolia.com/api/v1/search?query=${query}`);
+          event.preventDefault();
+        }}
       >
-        Search
-      </button>
-      <ul>
-        {data.hits.map(item => (
-          <li key={item.objectID}>
-            <a href={item.url}>{item.title}</a>
-          </li>
-        ))}
-      </ul>
+        <input
+          type="text"
+          value={query}
+          onChange={event => setQuery(event.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+      {isError && <div>Something went wrong...</div>}
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <ul>
+          {data.hits.map(item => (
+            <li key={item.objectID}>
+              <a href={item.url}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      )}
     </Fragment>
   );
 }
